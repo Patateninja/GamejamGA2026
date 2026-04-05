33466a46cba83ac3e2524424f90a8eb86e42ae1b
Patateninja <80772495+Patateninja@users.noreply.github.com>
2026-04-05 02:04:32 +0200

    Audio Files Imported + Door Audio

--- GamejamGA2026/Assets/Scripts/Door.cs
+++ GamejamGA2026/Assets/Scripts/Door.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.Audio;
 
 public class Door : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class Door : MonoBehaviour
 
     [SerializeField]
     private GameObject door;
+
+    [SerializeField]
+    private AudioSource audioSrc;
 
     private bool open = false;
 
@@ -19,13 +23,15 @@ public class Door : MonoBehaviour
 
     void Update()
     {
-        if (button.Where(b => !b.active).ToArray().Length == 0)
+        if (open == false && button.Where(b => !b.active).ToArray().Length == 0)
         {
             open = true;
+            audioSrc.PlayOneShot(audioSrc.clip);
         }
-        else
+        else if (open == true && button.Where(b => !b.active).ToArray().Length != 0)
         {
             open = false;
+            audioSrc.PlayOneShot(audioSrc.clip);
         }
 
         door.transform.position = Vector3.Lerp(door.transform.position, new Vector3(door.transform.position.x, open ? -.75f : .5f, door.transform.position.z), Time.deltaTime * 3f);
